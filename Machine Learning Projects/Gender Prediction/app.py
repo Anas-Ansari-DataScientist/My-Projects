8085a64e597c0850cc424873520760e04898c112
Md. Anas Ansari <54945647+Anas-coder@users.noreply.github.com>
2020-04-17 00:55:00 +0530

    Add files via upload

---
+++ Machine Learning Projects/Gender Prediction/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    if prediction == 0:
+        print('Female')
+    else:
+        print('Male')
+
+    return render_template('index.html', prediction_text='You are- {},If the prediction is 0 its a Female.....If the Prediction is 1 its a male'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
